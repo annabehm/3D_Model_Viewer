a31d0e3b7ea3aca89bef12a35fce0615d2a30bc0
annabehm <110179381+annabehm@users.noreply.github.com>
2023-10-17 15:32:16 -0230

    With Dropbox

--- README.md
+++ README.md
@@ -5,18 +5,9 @@ My name is Anna Behm (Student ID: 202005872). I hope you enjoy the application!
 To run this application:
 Go to the viewer.html file, ensure you have the Live Server Extension (if usiing Visual Studio Code), and click on the "Go Live" button on the bottom right. This will bring you to the html page where you can interact with the objects. 
 Click Zoom In or Zoom Out to see the object up close or from afar.
-To view the other models, you must go into the viewer.js file, make the changes to the objToRender variable, then save the file. The html page should automatically update.
+To view the other models, you can choose from the dropdown menu. There is a slight bug when you interact with the dino or phoenix models that the other models move position, which I aim to fix asap.
 
 
-To change the models, for the time being, you have to go into the viewer.js file and manually change
-let objToRender = "eye";
-To either 
-let objToRender = "dino";
-Or
-let objToRender = "phoenix";
-To see and interact with the two other 3D models I have in this application.
-
-
-This project has been very enjoyable, as you can see from the multiple addtions I have included. My next steps are to include a drop down menu to select the models from the html page, as well as animation for the phoenix.
+This project has been very enjoyable, as you can see from the multiple addtions I have included. My next steps are to include animation for the phoenix.
 
 Hope you enjoyed!
--- js/viewer.js
+++ js/viewer.js
@@ -1,13 +1,10 @@
-//Import the THREE.js library
+//Imports needed to make the 3D viewer, allow for interaction, and to load the GLTF file
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-// To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// To allow for importing the .gltf file
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-//Create a Three.JS Scene
+//Create scene and camera
 const scene = new THREE.Scene();
-//create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 //Keep track of the mouse position, so we can make the eye move
@@ -21,73 +18,137 @@ let controls;
 
 let objToRender = "eye";
 
+function loadModel(){
+  //Instantiate a loader for the .gltf file
+  const loader = new GLTFLoader();
+  //Load the file
+  loader.load(
+    `models/${objToRender}/scene.gltf`,
+    
+    function (gltf) {
+      //If the file is loaded, add it to the scene
+      object = gltf.scene;
+      //object.position.setX(50);
+      //Check if the model is the "phoenix", if so change the opacity and location so it looks better
+      if (objToRender === "phoenix"){
+        object.position.setY(-200);
+        object.traverse((child) => {
+          if (child.isMesh) {
+            // Reduce transparency by setting opacity to a value between 0 and 1
+            child.material.opacity = 1; // Adjust this value as needed
+            child.material.transparent = true; // Enable transparency
+            child.material.side = THREE.DoubleSide; // Render both sides of the mesh
+          }
+        });
+      }
+      
+      scene.add(object);
+      
+    },
+    function (xhr) {
+      //While it is loading, log the progress
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+      //If there is an error, log it
+      console.error(error);
+    }
+  );
+}
+loadModel();
 
-
-
+//This checks the dropdown menu and checks which model is selected, then changes the model to the chosen one
 document.getElementById("model-dropdown").addEventListener("change", function () {
   let modelDropdown = document.getElementById("model-dropdown");
   let selectedModel = modelDropdown.value;
+  camera.position.z=500;
   if (selectedModel == "eye"){
     objToRender = "eye"
+    console.log("eye")
+    
   }
   if (selectedModel == "dino"){
     objToRender = "dino"
+    console.log("dino")
+    camera.position.z = 25;
   }
   if (selectedModel == "phoenix"){
     objToRender = "phoenix"
+    console.log("phoenix")
   }
-});
 
-
-
-//Instantiate a loader for the .gltf file
-const loader = new GLTFLoader();
-
-//Load the file
-loader.load(
-  `models/${objToRender}/scene.gltf`,
-  function (gltf) {
-    //If the file is loaded, add it to the scene
-    object = gltf.scene;
-    
-    
-    if (objToRender === "phoenix"){
-      
-      object.position.setY(-200);
-      object.traverse((child) => {
-        if (child.isMesh) {
-          // Reduce transparency by setting opacity to a value between 0 and 1
-          child.material.opacity = 1; // Adjust this value as needed
-          child.material.transparent = true; // Enable transparency
-          child.material.side = THREE.DoubleSide; // Render both sides of the mesh
-        }
-      });
-      
-    }
-    
-
-    
-    scene.add(object);
-    
-  },
-  function (xhr) {
-    //While it is loading, log the progress
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  function (error) {
-    //If there is an error, log it
-    console.error(error);
+  // Remove existing object from scene
+  if (object) {
+    scene.remove(object);
   }
+
+  loadModel();
 
   
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth, window.innerHeight);
 
+//Add the renderer to the DOM
+document.getElementById("container3D").appendChild(renderer.domElement);
 
+//Set how far the camera will be from the 3D model
 
+camera.position.z = objToRender === "dino" ? 25 : 500;
 
-);
+//Controls for the camera, so we can rotate / zoom it with the mouse
+if (objToRender === "dino") {
+  controls = new OrbitControls(camera, renderer.domElement);
+}
+if (objToRender === "phoenix") {
+  controls = new OrbitControls(camera, renderer.domElement);
+}
 
+//Render the scene
+function animate() {
+  requestAnimationFrame(animate);
 
+  //Make the eye move
+  if (object && objToRender === "eye") {
+    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+  }
+  renderer.render(scene, camera);
+}
 
+//Add a listener to the window, to resize the window and the camera
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+//Add mouse position listener, to make the eye move
+document.onmousemove = (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+}
+
+//Zoom in and zoom out buttons
+const zoomInButton = document.getElementById('zoom-in-button');
+const zoomOutButton = document.getElementById('zoom-out-button');
+
+const zoomSpeed = 0.1; // Zoom speed
+
+//Zoom calculations and updating the page
+zoomInButton.addEventListener('click', () => {
+  camera.zoom *= 1 + zoomSpeed;
+  camera.updateProjectionMatrix();
+});
+
+zoomOutButton.addEventListener('click', () => {
+  camera.zoom /= 1 + zoomSpeed;
+  camera.updateProjectionMatrix();
+});
+
+//Begin 3D rendering
+animate();
+});
 
 
 
@@ -100,12 +161,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
+
 camera.position.z = objToRender === "dino" ? 25 : 500;
 
 
-//Add lights to the scene, so we can actually see the 3D model
+//Add lights to the scene, so we can see the 3D model
 const topLight = new THREE.DirectionalLight(0xffffff, 5); // (color, intensity)
-topLight.position.set(500, 500, 500) //top-left-ish
+topLight.position.set(500, 500, 500) 
 topLight.castShadow = true;
 scene.add(topLight);
 
@@ -114,11 +176,9 @@ scene.add(topLight);
 const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
 scene.add(ambientLight);
 
-//This adds controls to the camera, so we can rotate / zoom it with the mouse
+console.log(objToRender);
+//Controls for the camera, so we can rotate / zoom it with the mouse
 if (objToRender === "dino") {
-  controls = new OrbitControls(camera, renderer.domElement);
-}
-if (objToRender === "plane") {
   controls = new OrbitControls(camera, renderer.domElement);
 }
 if (objToRender === "phoenix") {
@@ -128,7 +188,6 @@ if (objToRender === "phoenix") {
 //Render the scene
 function animate() {
   requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
 
   //Make the eye move
   if (object && objToRender === "eye") {
@@ -138,29 +197,26 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-//Add a listener to the window, so we can resize the window and the camera
+//Add a listener to the window, to resize the window and the camera
 window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-//add mouse position listener, so we can make the eye move
+//Add mouse position listener, to make the eye move
 document.onmousemove = (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 }
 
-// js/main.js
-
-
-
-
+//Zoom in and zoom out buttons
 const zoomInButton = document.getElementById('zoom-in-button');
 const zoomOutButton = document.getElementById('zoom-out-button');
 
-const zoomSpeed = 0.1; // Adjust the zoom speed as needed
+const zoomSpeed = 0.1; // Zoom speed
 
+//Zoom calculations and updating the page
 zoomInButton.addEventListener('click', () => {
   camera.zoom *= 1 + zoomSpeed;
   camera.updateProjectionMatrix();
@@ -171,8 +227,5 @@ zoomOutButton.addEventListener('click', () => {
   camera.updateProjectionMatrix();
 });
 
-
-
-
-//Start the 3D rendering
+//Begin 3D rendering
 animate();
